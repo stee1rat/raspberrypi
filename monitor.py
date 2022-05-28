66fc5c02ab6becc1c6a7d52b22edc47f688d9310
stee1rat <aboymov@gmail.com>
2022-05-28 21:26:22 +0300

    initial commit

---
+++ monitor.py
@@ -0,0 +1,37 @@
+import board
+import requests
+import time
+
+from adafruit_htu21d import HTU21D
+from rpi_lcd import LCD
+
+def get_request(url):
+    try:
+        response = requests.get(url, timeout=5)
+        if response.status_code == 200:
+            return response
+        else:
+            return f"ERR:{response.status_code}"
+    except requests.exceptions.ConnectionError:
+        return "ERROR"
+    except requests.exceptions.ReadTimeout:
+        return "TIMEOUT"
+
+i2c = board.I2C()
+sensor = HTU21D(i2c)
+lcd = LCD(bus=3)
+
+last_updated = time.time() - 800
+
+while True:
+    if time.time() - last_updated > 600:
+        weather = get_request("http://wttr.in/Moscow?format=%x+%t")
+        if isinstance(weather, requests.models.Response):
+            weather = weather.text.replace("°","")
+        USD = get_request("https://rub.rate.sx/1USD")
+        if isinstance(USD, requests.models.Response):
+            USD = f"{float(USD.text):.2f}"
+        last_updated = time.time()
+    lcd.text(f"T:{sensor.temperature:.1f}C {weather}",  1)
+    lcd.text(f"H:{sensor.relative_humidity:.1f}% U:{USD}", 2)
+    time.sleep(10)
