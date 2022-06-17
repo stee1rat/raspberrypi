8c4a0b32b74061601744f6cf79d40434230caf9d
stee1rat <aboymov@gmail.com>
2022-06-17 20:18:17 +0300

    refactoring

--- stats.py
+++ stats.py
@@ -2,9 +2,7 @@ import Adafruit_SSD1306
 import subprocess
 import time
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+from PIL import Image, ImageDraw, ImageFont
 
 
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=4)
